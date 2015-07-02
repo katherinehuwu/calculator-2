98b0982eddbf6969e8643cc57a932d6a161085d0
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 17:23:20 -0700

    Printing works

--- calculator.py
+++ calculator.py
@@ -12,15 +12,17 @@ first_input = None
 while first_input != "q":
     user_input = raw_input("Please enter prefix notation math problem: ")
     math_dtl = user_input.split(" ")
+    answer = None
 
     if math_dtl[0] == "q":
         break
 
     elif math_dtl[0] == "+":
         answer = add(int(math_dtl[1]), int(math_dtl[2]))
-        print answer
 
     elif math_dtl[0] == "-":
         answer = subtract(int(math_dtl[1]), int(math_dtl[2]))
+        
+    if answer != None:
         print answer
-
+ 
